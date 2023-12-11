7094a71c1382a654ef25ea77db14157bba869e1f
Jose Quiñónez <Jquinonez@tsc.com.gt>
2023-12-11 14:37:09 -0600

    Add AsyncLazy.cs

--- Todo App/AsyncLazy.cs
+++ Todo App/AsyncLazy.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using AndroidX.AppCompat.View.Menu;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,21 @@ namespace Todo_App
 {
     public class AsyncLazy<T>
     {
+        readonly Lazy<Task<T>> instance;
+
+        public AsyncLazy(Func<T> factory)
+        {
+            instance = new Lazy<Task<T>>(() => Task.Run(factory));
+        }
+
+        public TaskAwaiter<T> GetAwaiter()
+        {
+            return instance.Value.GetAwaiter();
+        }
+
+        public void Start() 
+        { 
+            var unused = instance.Value;
+        }
     }
 }
