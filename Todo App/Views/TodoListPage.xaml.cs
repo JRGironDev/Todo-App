ccf9304e7d4e6f3504db12812c8e889ddc4a8cbc
Jose Quiñónez <Jquinonez@tsc.com.gt>
2023-12-11 16:11:40 -0600

    Add methods in the behind-code to TodoListPage

--- Todo App/Views/TodoListPage.xaml.cs
+++ Todo App/Views/TodoListPage.xaml.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using Todo_App.Data;
+using Todo_App.Models;
 
 namespace Todo_App.Views
 {
@@ -15,6 +14,32 @@ namespace Todo_App.Views
         public TodoListPage()
         {
             InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            TodoItemDatabase database = await TodoItemDatabase.Instance;
+            listView.ItemsSource = await database.GetItemsAsync();
+        }
+
+        async void OnItemAdded(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TodoItemPage
+            {
+                BindingContext = new TodoItem()
+            });
+        }
+
+        async void OnListItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new TodoItemPage
+                {
+                    BindingContext = e.SelectedItem as TodoItem
+                });
+            }
         }
     }
 }
