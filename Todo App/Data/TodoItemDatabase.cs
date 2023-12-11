ac64ffc9de3ec9f7e8c5b58e69ffd0b4829b9898
Jose Quiñónez <Jquinonez@tsc.com.gt>
2023-12-11 15:48:00 -0600

    Add TodoItemDatabase.cs

---
+++ Todo App/Data/TodoItemDatabase.cs
@@ -0,0 +1,30 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo_App.Data
+{
+    public class TodoItemDatabase
+    {
+        static SQLiteAsyncConnection Database;
+
+        public static readonly AsyncLazy<TodoItemDatabase> Instance =
+            new AsyncLazy<TodoItemDatabase>(async () =>
+            {
+                var instance = new TodoItemDatabase();
+                try
+                {
+                    CreateTableResult result = await Database.CreateTableAsync<TodoItem>();
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+                return instance;
+            });
+    }
+}
